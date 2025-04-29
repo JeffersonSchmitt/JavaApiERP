4255f3e9afc4cb8baf1d3867621196fa1af58bfe
Jefferson Schmitt <jeffersonschmitt@live.com>
2025-04-29 15:29:21 -0300

    Correção do post e put do usuario

--- src/main/java/com/example/FirstApi/controller/UserController.java
+++ src/main/java/com/example/FirstApi/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.example.FirstApi.controller;
 
 import com.example.FirstApi.controller.Interface.IBaseController;
-import com.example.FirstApi.dataObject.UserDTO;
+import com.example.FirstApi.model.user.User;
 import com.example.FirstApi.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,35 +15,37 @@ import java.util.List;
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 @Tag(name = "User", description = "Endpoints de usuário")
-public class UserController implements IBaseController<UserDTO> {
+public class UserController implements IBaseController<User> {
 
     private final UserService userService;
 
     @GetMapping
     @Operation(summary = "Listar todos os usuarios")
     @Override
-    public List<UserDTO> getAll() {
+    public List<User> getAll() {
         return userService.findAll();
     }
 
     @GetMapping("/{id}")
     @Operation(summary = "Buscar usuarios por ID")
     @Override
-    public ResponseEntity<UserDTO> getById(@PathVariable Long id) {
-        UserDTO userDTO = userService.findById(id);
-        return userDTO != null ? ResponseEntity.ok(userDTO) : ResponseEntity.notFound().build();
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
     }
 
     @PostMapping
     @Operation(summary = "Cadastrar novo usuario")
     @Override
-    public UserDTO create(UserDTO userDTO) {
-        return userService.save(userDTO);
+    public User create(@RequestBody User user) {
+        return userService.save(user);
     }
 
-    public ResponseEntity<UserDTO> update(Long id, @RequestBody UserDTO userDTO) {
-        UserDTO updatedUserDTO = userService.update(id, userDTO);
-        return updatedUserDTO != null ? ResponseEntity.ok(updatedUserDTO) : ResponseEntity.notFound().build();
+    @PutMapping("/{id}")
+    @Operation(summary = "Atualizar cliente existente")
+    public ResponseEntity<User> update(Long id, @RequestBody User user) {
+        User updatedUser = userService.update(id, user);
+        return updatedUser != null ? ResponseEntity.ok(updatedUser) : ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/example/FirstApi/service/UserService.java
+++ src/main/java/com/example/FirstApi/service/UserService.java
@@ -1,10 +1,9 @@
 package com.example.FirstApi.service;
 
-import com.example.FirstApi.dataObject.UserDTO;
-import com.example.FirstApi.dataObject.mapper.UserMapper;
 import com.example.FirstApi.model.user.User;
-import com.example.FirstApi.repository.IUserRepository;
 import com.example.FirstApi.service.Interface.IBaseService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -14,23 +13,22 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class UserService implements IBaseService<UserDTO> {
+public class UserService implements IBaseService<User> {
     private final RestTemplate restTemplate;
     private final String URL_BASE = "https://fakestoreapi.com/users/";
 
     @Override
-    public List<UserDTO> findAll() {
+    public List<User> findAll() {
 
         try {
             User[] users = restTemplate.getForObject(URL_BASE, User[].class);
             if (users != null) {
-                return Arrays.stream(users)
-                        .map(UserMapper::toDTO)
-                        .collect(Collectors.toList());
+                return Arrays.asList(users);
             }
             return null;
         } catch (Exception e) {
@@ -39,35 +37,33 @@ public class UserService implements IBaseService<UserDTO> {
     }
 
     @Override
-    public UserDTO findById(Long id) {
+    public User findById(Long id) {
         String url = URL_BASE + id;
-
         try {
             User user = restTemplate.getForObject(url, User.class);
-            return user != null ? UserMapper.toDTO(user) : null;
+            return Objects.requireNonNull(user).getId() != null ? user : null;
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
     @Override
-    public UserDTO save(UserDTO userDT) {
-        User user = UserMapper.toEntity(userDT);
+    public User save(User user) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<User> request = new HttpEntity<>(user, headers);
-        User createdUser = restTemplate.postForObject(URL_BASE, request, User.class);
 
-        return createdUser != null ? UserMapper.toDTO(createdUser) : null;
+        User createdUser = restTemplate.postForObject(URL_BASE, request, User.class);
+        User completeUser = findById(createdUser.getId());
+
+        return completeUser;
     }
 
     @Override
-    public UserDTO update(Long id, UserDTO userDTO) {
-        UserDTO existingUser = findById(id);
-        User updatedUser = UserMapper.toEntity(userDTO);
-        restTemplate.put(URL_BASE + id, updatedUser);
-
-        return existingUser != null ? UserMapper.toDTO(updatedUser) : null;
+    public User update(Long id, User userDTO) {
+        User existingUser = findById(id);
+        restTemplate.put(URL_BASE + id, existingUser);
+        return Objects.requireNonNull(existingUser).getName() != null ? existingUser : null;
     }
 
     @Override
